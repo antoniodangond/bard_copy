27ffccf4711d90d6705809732072fa6a77057bd0
Paul Naser <p.naser74@gmail.com>
2025-10-02 23:21:07 -0700

    Directional Attack
    Main/starting attack is now directional and fires in the direction the player is facing

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class WeaponController : MonoBehaviour
 {
     public float AttackRange;
+    private Vector2 attackRangeV2;
     public LayerMask enemyLayer;
     public PlayerController playerController;
     private Vector2 attackDirection;
@@ -40,9 +41,29 @@ public class WeaponController : MonoBehaviour
         }
     }
 
+    private Vector2 getAttackRangeV2() {
+        switch (PlayerController.FacingDirection)
+        {
+            case FacingDirection.Up:
+                return new Vector2(1, AttackRange + 2);
+            // gameObject.transform.position = transformValue + new Vector3(0, 2, 0);
+            case FacingDirection.Down:
+                return new Vector2(1, AttackRange * -1);
+            // gameObject.transform.position = transformValue - new Vector3(0, 1, 0);
+            case FacingDirection.Left:
+                return new Vector2((AttackRange * -1) - 1, 1);
+            // gameObject.transform.position = transformValue - new Vector3(1, 0, 0) + new Vector3 (0,0.5f,0);
+            case FacingDirection.Right:
+                return new Vector2(AttackRange+ 1, 1);
+            // gameObject.transform.position = transformValue + new Vector3(1, 0.5f, 0);
+            default:
+                return new Vector2(0, 0);
+        }
+    }
     public void Attack()
     {
-        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, AttackRange, enemyLayer);
+        attackRangeV2 = getAttackRangeV2();
+        Collider2D[] hitColliders = Physics2D.OverlapBoxAll(transform.position, attackRangeV2, 0, enemyLayer);
         foreach (Collider2D hitCollider in hitColliders)
         {
             // Ignore enemy trigger colliders, which are just used for agro range
@@ -60,10 +81,33 @@ public class WeaponController : MonoBehaviour
             enemyController.TakeDamage(damage, playerController, attackDirection);
         }
     }
+    // saving old code for attack as it. This will be the AOE attack that the player will get in the dungeon
+    // will be renamed accordingly
+    // public void Attack()
+    // {
+    //     Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, AttackRange, enemyLayer);
+    //     foreach (Collider2D hitCollider in hitColliders)
+    //     {
+    //         // Ignore enemy trigger colliders, which are just used for agro range
+    //         if (hitCollider.isTrigger)
+    //         {
+    //             continue;
+    //         }
+    //         if (PlayerInputManager.Movement == new Vector2(0, 0))
+    //         {
+    //             attackDirection = PlayerController.getFacingDirectionVector2();
+    //         }
+    //         else { attackDirection = PlayerInputManager.Movement; }
+    //         // Vector2 attackDirection = PlayerController.getFacingDirectionVector2(PlayerController.FacingDirection);
+    //         EnemyController enemyController = hitCollider.GetComponent<EnemyController>();
+    //         enemyController.TakeDamage(damage, playerController, attackDirection);
+    //     }
+    // }
 
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.magenta;
-        Gizmos.DrawWireSphere(transform.position, AttackRange);
+        Vector3 attackRangeV3 = attackRangeV2;
+        Gizmos.DrawWireCube(transform.position, attackRangeV3);
     }
 }
