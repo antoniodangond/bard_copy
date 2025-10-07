4089f69e93e8cbafcad9fc047953dba21b04baa0
Paul Naser <p.naser74@gmail.com>
2025-10-06 23:14:44 -0700

    item pickup step 2
    item pickup script added to gameobjects. They can now be "collected" (they disappear when collided with)

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -1,16 +1,33 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class ItemPickup : MonoBehaviour
 {
+    private BoxCollider2D boxCollider2D;
+    private SpriteRenderer spriteRenderer;
+    public string itemName;
+    // this should be a reference to some more global tracker
+    private bool collected;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        collected = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collected = true;
+        Debug.Log($"It's {collected}, you picked up the item!");
+        spriteRenderer.enabled = false;
+        // Destroy(gameObject);
     }
 }
