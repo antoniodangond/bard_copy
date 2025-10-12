80c30890cb71bbbe21d977c0d033119dc6957ed3
Paul Naser <p.naser74@gmail.com>
2025-10-11 17:20:38 -0700

    particle emission on attack

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -43,7 +43,8 @@ public class WeaponController : MonoBehaviour
         }
     }
 
-    private Collider2D[] getHitColliders() {
+    private Collider2D[] getHitColliders()
+    {
         switch (PlayerController.FacingDirection)
         {
             case FacingDirection.Up:
@@ -89,11 +90,66 @@ public class WeaponController : MonoBehaviour
     //             return new Vector2(0, 0);
     //     }
     // }
+    private Vector2 getAttackDirection() 
+    {
+        if (PlayerInputManager.Movement == new Vector2(0, 0)) { return PlayerController.getFacingDirectionVector2(); }
+        else { return PlayerInputManager.Movement; }
+    }
+    private void handleAttackParticles()
+    {
+        // Spawn particles halfway between player and end of attack and increase height to be away from Player's feet
+        Vector3 heightOffset;
+        Vector3 eulerAngles = gameObject.transform.rotation.eulerAngles;
+        Quaternion particleRotation;
+        // Vector3 particleSpawnOffset = (Vector3)(attackDirection.normalized * (AttackRange / 2f)) + new Vector3(0, 1.5f, 0);
+        switch (PlayerController.FacingDirection)
+        {
+            case FacingDirection.Up:
+                heightOffset = new Vector3(0, 0, 0);
+                eulerAngles.z += 70f;
+                particleRotation = Quaternion.Euler(eulerAngles);
+                break;
+            case FacingDirection.Down:
+                heightOffset = new Vector3(0, 0, 0);
+                eulerAngles.z -= 110f;
+                particleRotation = Quaternion.Euler(eulerAngles);
+                break;
+            case FacingDirection.Left:
+                heightOffset = new Vector3(0, 1.5f, 0);
+                eulerAngles.z += 210f;
+                particleRotation = Quaternion.Euler(eulerAngles);
+                break;
+            case FacingDirection.Right:
+                heightOffset = new Vector3(0, 1.5f, 0);
+                particleRotation = Quaternion.Euler(eulerAngles);
+                break;
+            default:
+                heightOffset = new Vector3(0, 1, 0);
+                particleRotation = Quaternion.Euler(eulerAngles);
+                break;
+        }
+        attackDirection = getAttackDirection();
+        Vector3 particleSpawnOffset = (Vector3) attackDirection + heightOffset;
+        // Vector3 particleSpawnPosition = transform.position + particleSpawnOffset;
+        Vector3 particleSpawnPosition = transform.position + heightOffset;
+        // attackParticles.velocityOverLifetime.x
+
+        // Rotate particle system to face the attack direction
+        // float angle = Mathf.Atan2(attackDirection.y, attackDirection.x) * Mathf.Rad2Deg;
+        // Quaternion particleRotation = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
+
+       
+
+
+
+        attackParticlesInstance = Instantiate(attackParticles, particleSpawnPosition, particleRotation);
+        // attackParticlesInstance = Instantiate(attackParticles, particleSpawnPosition, playerController.transform.rotation);
+    }
     public void Attack()
     {
         // attackRangeV2 = getAttackRangeV2();
 
-        // Collider2D[] hitColliders = Physics2D.OverlapBoxAll(transform.position, attackRangeV2, 0, enemyLayer);
+        attackDirection = getAttackDirection();
         Collider2D[] hitColliders = getHitColliders();
         foreach (Collider2D hitCollider in hitColliders)
         {
@@ -102,20 +158,11 @@ public class WeaponController : MonoBehaviour
             {
                 continue;
             }
-            if (PlayerInputManager.Movement == new Vector2(0, 0))
-            {
-                attackDirection = PlayerController.getFacingDirectionVector2();
-            }
-            else { attackDirection = PlayerInputManager.Movement; }
-            // Vector2 attackDirection = PlayerController.getFacingDirectionVector2(PlayerController.FacingDirection);
+            
             EnemyController enemyController = hitCollider.GetComponent<EnemyController>();
             enemyController.TakeDamage(damage, playerController, attackDirection);
-            // Quaternion spawnRotation_1 = Quaternion.FromToRotation(movement * -1, movement);
         }
-        Vector3 attackDirectionV3 = attackDirection;
-        Debug.Log(attackParticles);
-        // spawn particles in same direction as player attack but higher up since transform position is at player's feet
-        attackParticlesInstance = Instantiate(attackParticles, transform.position + (attackDirectionV3*2) + new Vector3 (0,1.5f,0), playerController.transform.rotation);
+        handleAttackParticles();
     }
     // saving old code for attack as it. This will be the AOE attack that the player will get in the dungeon
     // will be renamed accordingly
