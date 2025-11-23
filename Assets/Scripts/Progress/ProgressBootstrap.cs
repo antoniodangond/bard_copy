27f8160e36151bbf1bdf0f18a6a0a231ce546996
Paul Naser <p.naser74@gmail.com>
2025-11-23 12:01:04 -0800

    fix audio bug

--- Assets/Scripts/Progress/ProgressBootstrap.cs
+++ Assets/Scripts/Progress/ProgressBootstrap.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class ProgressBootstrap : MonoBehaviour
 {
+    public GameObject gameManager;
     void Awake()
     {
         if (PlayerProgress.Instance == null)
         {
-            var go = new GameObject("PlayerProgress");
-            go.AddComponent<PlayerProgress>(); // becomes DontDestroyOnLoad automatically
+            var go = new GameObject();
+            // go.AddComponent<GameManager>(); // becomes DontDestroyOnLoad automatically
+            go = Instantiate(gameManager, new Vector3(0,0,0), Quaternion.identity);
         }
     }
 }
