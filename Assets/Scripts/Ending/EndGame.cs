e37a20e8bf0c809b293b20b8700b6f18803cee17
Paul Naser <p.naser74@gmail.com>
2026-01-09 21:18:22 -0800

    work on bad ending

---
+++ Assets/Scripts/Ending/EndGame.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BadEnd : MonoBehaviour
+{
+    public Image imageComponent;
+    public EndingData endingData;
+    public float imageCycleTime;
+    public enum endingType
+    {
+        bad
+        , good
+    }
+    public endingType ending;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PlayEnding();
+    }
+
+    private void PlayEnding()
+    {
+        if (ending == endingType.bad)
+        {
+            Sprite[] sprites = endingData.badEndingSprites;
+            // imageComponent.sprite = sprites[0];
+            StartCoroutine(PlayEndRoutine(sprites, imageCycleTime));
+        }
+    }
+    
+    private IEnumerator PlayEndRoutine(Sprite[] sprites,float imageCycleTime)
+    {
+        int cycles = sprites.Length;
+
+        for (int i = 0; i < cycles; i++)
+        {
+            imageComponent.sprite = sprites[i];
+            yield return new WaitForSeconds(imageCycleTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Ending/EndingData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "endData", menuName = "endingData")]
+public class EndingData : ScriptableObject
+{
+    public Sprite[] badEndingSprites;
+    public Sprite[] goodEndingSprites;
+}
